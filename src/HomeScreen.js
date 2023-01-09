53b9dc3e95bb5f52df66358720e6575c146a47f8
Shahnawaz Alam <snawaza243@gmail.com>
2023-01-10 02:31:25 +0530

    update 2

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './HomeScreen.css'
+import Nav from "./Nav";
+function HomeScreen(){
+    return(
+        <div className="homeScreen">
+            <h2>Home screen</h2>
+            <Nav />
+
+            {/* Banner */}
+
+            {/* Raw */}
+
+        </div>
+    )
+}
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQYpdgD-xz9YXn7eANFS2Lot7xUV3xttU",
+  authDomain: "react-netflix-clone-214b6.firebaseapp.com",
+  projectId: "react-netflix-clone-214b6",
+  storageBucket: "react-netflix-clone-214b6.appspot.com",
+  messagingSenderId: "1063988597273",
+  appId: "1:1063988597273:web:bd78ea0429e2700f501088"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
